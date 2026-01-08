7b911a309e40cc93c593c8ce048151a1c54c5acf
Rohit <rohitchaudharirc375@gmail.com>
2026-01-08 10:09:52 +0530

    Add files via upload

---
+++ Async.js
@@ -0,0 +1,216 @@
+/* // this script is for asynchronous practice 
+
+//when do we use async programming 
+// what is async 
+// practice arrow function 
+
+/*
+console.log('one')
+console.log('two')
+
+setTimeout(() => {
+    console.log('hello')
+}, 2000);
+
+console.log('three')
+console.log('four')
+*/
+
+// callback is an argument to an another function 
+// A callback is a function that is passed as an argument to another function and is executed later, usually after an asynchronous task finishes.
+
+/*  
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+function processUser(callback) {
+  callback("Rohit");
+}
+*/
+
+// processUser(greet);
+
+
+function greet(name){
+    console.log('hello' +' '+ name)
+}
+
+function user(callback){
+    console.log('processing user.....')
+
+    setTimeout(function(){
+        callback('rohit')
+    }, 3000)
+}
+
+user(greet)
+
+
+
+function sum(a, b) {
+    console.log(a + b);
+}
+
+function calculator(a, b, sumcallback) {
+    sumcallback(a, b);
+}
+
+calculator(5, 3, sum);
+
+// callback hell 
+/* 
+function getdata(dataid, getNextData){
+    setTimeout(()=>{
+        console.log("data",dataid)
+        if (getNextData){
+            getNextData();
+        }
+    },3000)
+}
+
+getdata(1,()=> {
+    getdata(2,()=>{
+        getdata(3, () =>{
+           getdata(4) 
+        });
+    });
+});
+
+*/
+// complex to understand and manage -it is problem in javascript therfore we use promises 
+
+
+//PROMISES 
+
+/*
+let promise = new Promise((resolve, reject) => {
+    console.log("i am a promise ")
+    //resolve('delivered') // function created by javascript 
+    reject('some error')
+})
+*/ 
+
+/*
+function getdata(dataid, getNextData){
+    return new Promise((resolve, reject) => {
+        setTimeout (()=> {
+            console.log("data", dataid)
+            resolve('delivered')
+            if(getNextData){
+                getNextData()
+            }
+        },3000)
+    })
+}
+*/
+
+
+// has 3 states pending, fullfilled(resolved) and rejected 
+
+
+// using .then and .catch() in promises, .then used when fulfilled and .catch used when a promise is rejected 
+
+
+/* 
+const getpromise = ()=> {
+    return new Promise((resolve, reject) => {
+        console.log('i am a promise')
+        //resolve('sucess')
+        reject("error 101")
+    })
+}
+
+let promise = getpromise();
+promise.then((res)=> { 
+    console.log('promise fulfilled', res)
+})
+
+promise.catch((err)=>{
+    console.log('rejected', err)
+})
+*/
+
+// Promise chaining 
+
+function asyncfunc1(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            console.log("data1")
+            resolve('sucess')
+        },4000)
+    })
+}
+
+function asyncfunc2(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            console.log("data1")
+            resolve('sucess')
+        },4000)
+    })
+}
+/*
+
+console.log('fetching data one')
+let p1 = asyncfunc1();
+p1.then((res)=> {
+    console.log(res)
+    console.log("fetching data 2 ")
+    let p2 =  asyncfunc2()
+    p2.then((res)=>{
+        console.log(res)
+    })
+})
+*/
+// can nbe called directly through the function 
+// promise chaining 
+console.log('fetching data1')
+asyncfunc1().then((res)=>{
+    console.log("fecthing data2 ")
+    asyncfunc2().then((res)=>{
+        console.log("fecthed complete record", res )
+    })
+})
+
+
+
+// asyc and await
+// async function returns a promise 
+
+
+/*
+async function hello1(){  // only for syntax purposes 
+    console.log('hello rohit')
+}
+// returns promise directly 
+*/
+
+function api(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            console.log('weather data')
+            resolve (200)
+        },2000)
+    })
+}
+
+async function getweatherdata(){ // call in console
+    await api() // 1st call  
+    await api() // 2nd call 
+}
+
+
+// await pauses the execution of its surrounding async function until the promise is setteled 
+// READ IIFE -  immediately invoked function expression  */
+
+
+
+
+
+
+
+
+
+
+
---
+++ practice.js
@@ -0,0 +1,167 @@
+/* console.log('this script is for async practice')
+
+
+function callme(){
+    console.log('bruger has arrived')
+}
+
+function orderburger(callback){
+    console.log("ordering burger")
+    setTimeout(()=>{
+        callback()
+    },3000)
+}
+
+
+orderburger(callme)
+
+
+
+function checkSum(a,b){
+    console.log(a+b)
+}
+
+checkSum(5,6)
+
+
+function checkEO(x){
+    if (x % 2 == 0 ){
+        return x + "the number is even"
+    }else {
+        return x + " the given numer is odd"
+    }
+}
+
+checkEO(45)
+
+*/
+
+
+// counting how many times a number appears 
+
+let t = [1,2,3,4,2,3,4,4]
+let target = 2
+let count_t = 0
+
+for (i = 0; i < t.length; i++){
+    if (t[i] === target ){
+        count_t ++;
+    }
+}
+
+console.log(count_t)
+
+
+// calculating sum of array 
+
+nums = [2,3,6,8,12]
+total = 0 
+
+for (i=0;i < nums.length;i++){
+    total += nums[i] 
+}
+
+console.log(total)
+
+
+// finding the largest number 
+
+val = [10,45,23,9,12]
+n_l = 0
+
+
+for (i=0; i<val.length;i++){
+    if (val[i] > n_l){
+        n_l = val[i]
+    }
+}
+
+console.log(n_l)
+
+
+//  count even numbers
+
+val_1 = [2,3,412,3,85,97,12]
+count_even = 0 
+
+for (i=0; i<val_1.length;i++){
+    if ( val_1[i] % 2 == 0){
+        count_even ++
+    } 
+}
+
+console.log(count_even)
+
+// remove duplicates 
+
+arr_1 = [1,2,3,4,5,6,7,8]
+uniqueArr = [] 
+
+for (i=0; i < arr_1.length ; i++){
+    if ( uniqueArr.includes(arr_1[i])){
+        uniqueArr.push(arr[i])
+    }
+}
+// another logic - try creating an set of uniques elements from the arr loop the unique arr with the arr and push the value in new ones 
+console.log(uniqueArr)
+
+
+// find number greater than 10 and smaller than 10 - make a list 
+
+value_1  = [23,4,5,6,5,25,43,4,534,6,56,57,6,7,64,45,4,67,6,58]
+num_list = []
+num_list_2 = []
+for (i=0;i<value_1.length;i++){
+    if (value_1[i] > 10){
+        num_list.push(value_1[i])
+    } else{
+        num_list_2.push(value_1[i])
+    }
+
+}
+console.log('list of number greater than 10',num_list)
+console.log('list of number less than 10',num_list_2)
+
+
+// count vowels in a string  
+
+
+str =  'my name is rohit, I love playing football'
+vowels = 'aieou'
+count_a = 0 
+
+for (i=0;i < str.length;i++){
+    if (vowels.includes(str[i])){
+        count_a ++
+    }
+}
+
+console.log(count_a)
+
+// find the longest word in a sentance 
+
+let sentence = "JavaScript makes web development powerful"
+
+let words = sentence.split(' ')
+let longestWord = ""
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+        longestWord = words[i]
+    }
+}
+
+console.log(longestWord)
+
+
+
+
+
+
+
+
+
+
+
+
+
